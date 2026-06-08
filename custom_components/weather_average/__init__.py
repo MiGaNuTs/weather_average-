cf9058609d7e92b0273a5943088c5baa60d8bf3f
MiGaNuTs <140328186+MiGaNuTs@users.noreply.github.com>
2026-06-08 21:38:13 +0200

    Add files via upload

--- custom_components/weather_average/__init__.py
+++ custom_components/weather_average/__init__.py
@@ -9,16 +9,9 @@ PLATFORMS = ["weather"]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Weather Average from a config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry when options change."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
